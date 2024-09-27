239cc72bfc230359ba2f78fb67cf63cc1214e0d1
deependu <deependujha21@gmail.com>
2024-09-27 11:50:28 +0530

    basic cli done

---
+++ src/litserve/cli/hello_world.py
@@ -0,0 +1,7 @@
+import sys
+
+def hello_world():
+    """Hello world from LitServe cli
+    This is a sample function to demonstrate LitServe cli
+    """
+    print("Hello World! from LitServe cli", __name__, sys.argv)
